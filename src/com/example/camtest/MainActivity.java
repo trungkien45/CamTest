1b9fc039f118617b2fe5607f8b4f006d9225f63c
trungkien45 <ntkien45-diendan@yahoo.com.vn>
2014-03-09 21:54:04 +0700

    Com

--- src/com/example/camtest/MainActivity.java
+++ src/com/example/camtest/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.camtest;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Size;
@@ -21,7 +25,7 @@ public class MainActivity extends Activity {
 	
 	int numberOfCameras;
 	int defaultCameraId;
-	ImageView imgVieW;
+	//ImageView imgVieW;
 	Camera mCamera;
 	MenuItem SaveMnuItem;
     int cameraCurrentlyLocked;
@@ -34,7 +38,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		numberOfCameras = Camera.getNumberOfCameras();
-		imgVieW= (ImageView)findViewById(R.id.imageView1);
+		//imgVieW= (ImageView)findViewById(R.id.imageView1);
 		view = (SurfaceView)findViewById(R.id.surfaceView1);
 		holder= view.getHolder();
 		mCamera = Camera.open();
@@ -94,48 +98,7 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    	//mCamera.setDisplayOrientation(90);
-    	mCamera.setPreviewCallback(new Camera.PreviewCallback() {
-			@Override
-			public void onPreviewFrame(byte[] data, Camera camera) {
-				// TODO Auto-generated method stub
-				//Point cameraResolution = mCamera.getCameraResolution();
-			    if (data != null) {
-			        //Log.i("DEBUG", "data Not Null");
-	 
-	                // Preprocessing
-			        //Log.i("DEBUG", "Try For Image Processing");
-	                Camera.Parameters mParameters = camera.getParameters();
-	                Size mSize = mParameters.getPreviewSize();
-	                int mWidth = mSize.width;
-	                int mHeight = mSize.height;
-	                int[] mIntArray = new int[mWidth * mHeight];
-	 
-	                // Decode Yuv data to integer array
-	                utylitis.decodeYUV420SP(mIntArray, data, mWidth, mHeight);
-	 
-	                // Converting int mIntArray to Bitmap and
-	                // than image preprocessing
-	                // and back to mIntArray.
-	                Bitmap bitmap= Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
-	                bitmap.setPixels(mIntArray, 0, mWidth, 0, 0, mWidth, mHeight);
-	                for(int i=0;i<mWidth;i++)
-	                	for(int j=0;j<mHeight;j++){
-	                		int a= bitmap.getPixel(i, j);
-	                		int x= (int)(Color.red(a)*0.3+Color.green(a)*0.59+Color.blue(a)*0.11);
-	                		bitmap.setPixel(i, j, Color.argb(255, x,x ,x));
-	                	}
-	                bitmap.getPixels(mIntArray, 0, mWidth, 0, 0, mWidth, mHeight);
-	                imgVieW.setImageBitmap(bitmap);
-	                // Encode intArray to Yuv data
-	                utylitis.encodeYUV420SP(data, mIntArray, mWidth, mHeight);
-	                camera.addCallbackBuffer(data);
-			    }
-
-			}
-		});
-    }
+     }
 
 	@Override
     protected void onResume() {
